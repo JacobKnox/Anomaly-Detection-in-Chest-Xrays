2cf2859e28d7cc142b61fb628d59718db64a8618
Jacob Knox <jacobknoxa@gmail.com>
2023-11-08 21:55:29 -0500

    Fix some issues
    I was testing it with a subset of the dataset to see if it would even load properly. I fixed issues related to that, but now I need to fix issues related to working with the whole dataset.

--- Models/fcn.py
+++ Models/fcn.py
@@ -1,5 +1,6 @@
 from typing import Any
 from keras import layers, Model
+import tensorflow as tf
 import numpy as np
 from PIL import Image
 from tqdm import tqdm
@@ -8,11 +9,21 @@ import pdb
 
 
 class FCN:
-    def __init__(self, num_classes: int = 17, dropout_rate: float = 0.2, filter: int = 32):
+    def __init__(
+        self, num_classes: int = 17, dropout_rate: float = 0.2, filter: int = 32
+    ):
         self.dropout_rate = dropout_rate
         self.num_classes = num_classes
         self.filter = filter
         self.model = self._make_model()
+        self.model.compile(
+            optimizer=tf.keras.optimizers.Adam(learning_rate=1e-3),
+            loss=tf.keras.losses.BinaryCrossentropy(),
+            metrics=[
+                tf.keras.metrics.BinaryAccuracy(),
+                tf.keras.metrics.FalseNegatives(),
+            ],
+        )
 
     def predict(self, input):
         return self.model.predict(input)
@@ -22,10 +33,10 @@ class FCN:
 
     def summary(self) -> None:
         print(self.model.summary())
-        print(f'Total number of layers: {len(self.model.layers)}')
+        print(f"Total number of layers: {len(self.model.layers)}")
 
     def _make_model(self) -> Model:
-        input = layers.Input(shape=(None, None, 3))
+        input = layers.Input(shape=(None, None, 1))
 
         group_one = self._group(input, self.filter, 1)
         group_two = self._group(group_one, self.filter * 2, 1)
@@ -35,13 +46,23 @@ class FCN:
         fully_connected_one = self._group(group_five, self.filter * 2, 1, 1)
 
         predictions = self._group(
-            fully_connected_one, self.num_classes, 1, 1, True, 'softmax')
+            fully_connected_one, self.num_classes, 1, 1, True, "softmax"
+        )
 
         return Model(inputs=input, outputs=predictions)
 
-    def _group(self, previous_layer: Any | list | None, filter: int, stride: int, kernel: int = 3, max_pool: bool = False, activation: str = "relu") -> Any | None:
-        layer = layers.Conv2D(
-            filters=filter, kernel_size=kernel, strides=stride)(previous_layer)
+    def _group(
+        self,
+        previous_layer: Any | list | None,
+        filter: int,
+        stride: int,
+        kernel: int = 3,
+        max_pool: bool = False,
+        activation: str = "relu",
+    ) -> Any | None:
+        layer = layers.Conv2D(filters=filter, kernel_size=kernel, strides=stride)(
+            previous_layer
+        )
         layer = layers.Dropout(self.dropout_rate)(layer)
         layer = layers.BatchNormalization()(layer)
         if max_pool:
@@ -52,21 +73,39 @@ class FCN:
 
 if __name__ == "__main__":
     data_dir = "C:\\Users\\epicd\\Documents\\Data"
-    FCN()
+    my_model = FCN()
+    my_model.summary()
     data_info = np.loadtxt(
-        "C:\\Users\\epicd\\Documents\\GitHub\\Anomaly-Detection-in-Chest-Xrays\\image_docs.csv", dtype=str, delimiter=',', skiprows=1)
+        "C:\\Users\\epicd\\Documents\\GitHub\\Anomaly-Detection-in-Chest-Xrays\\image_docs.csv",
+        dtype=str,
+        delimiter=",",
+        skiprows=1,
+    )
     train_data_info = data_info[np.where(data_info[:, 2] == "TRAIN")]
-    train_labels = train_data_info[:, 1]
+    _, train_labels = np.unique(train_data_info[:, 1], return_inverse=True)
+    train_labels += 1
     train_data = []
     if os.path.exists(".\\Data\\train.npy"):
         train_data = np.load(".\\Data\\train.npy")
     else:
+        i = -1
         for row in tqdm(train_data_info, desc="Loading training images", unit="image"):
+            i += 1
+            if i % 100 != 0:
+                continue
             image = Image.open(f"{data_dir}\\{row[1]}\\{row[2]}\\{row[0]}")
+            if len(np.asarray(image).shape) > 2:
+                train_labels = np.delete(train_labels, i)
+                continue
+                # temp = np.asarray(image)
+                # print(temp)
+                # pdb.set_trace()
             train_data.append(np.asarray(image))
-        np.save(".\\Data\\train.npy", train_data)
+        # np.save(".\\Data\\train.npy", train_data)
+    my_model.fit(train_data, train_labels)
     test_data_info = data_info[np.where(data_info[:, 2] == "TEST")]
-    test_labels = test_data_info[:, 1]
+    _, test_labels = np.unique(test_data_info[:, 1], return_inverse=True)
+    test_labels += 1
     test_data = []
     if os.path.exists(".\\Data\\test.npy"):
         train_data = np.load(".\\Data\\test.npy")
@@ -74,9 +113,12 @@ if __name__ == "__main__":
         for row in tqdm(test_data_info, desc="Loading testing images", unit="image"):
             image = Image.open(f"{data_dir}\\{row[1]}\\{row[2]}\\{row[0]}")
             test_data.append(np.asarray(image))
-        np.save(".\\Data\\test.npy", test_data)
+        # np.save(".\\Data\\test.npy", test_data)
+    prediction = my_model.predict(test_data)
+    print(f"Accuracy: {sum(prediction == test_labels)/len(test_labels)}")
     val_data_info = data_info[np.where(data_info[:, 2] == "VALIDATION")]
-    val_labels = val_data_info[:, 1]
+    _, val_labels = np.unique(val_data_info[:, 1], return_inverse=True)
+    val_labels += 1
     val_data = []
     if os.path.exists(".\\Data\\val.npy"):
         train_data = np.load(".\\Data\\val.npy")
@@ -84,5 +126,5 @@ if __name__ == "__main__":
         for row in tqdm(val_data_info, desc="Loading validation images", unit="image"):
             image = Image.open(f"{data_dir}\\{row[1]}\\{row[2]}\\{row[0]}")
             val_data.append(np.asarray(image))
-        np.save(".\\Data\\val.npy", val_data)
+        # np.save(".\\Data\\val.npy", val_data)
     pdb.set_trace()
